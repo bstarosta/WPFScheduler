bae596b241c5c492cbe0bf5278e789250da4183c
Bartosz Starosta <bstarosta77@gmail.com>
2020-04-15 17:45:42 +0200

    Added weather API communication

---
+++ WPFSchedulerTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using WPFScheduler;
+
+namespace WPFSchedulerTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
